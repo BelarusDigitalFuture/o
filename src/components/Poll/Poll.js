c4d11e8949a8c78525fd8a701f5508771f8d97a6
Andrei Bezmen <andryshabez26@gmail.com>
2021-02-08 17:11:17 +0300

    feat: rebuild component Poll

--- src/components/Poll/Poll.js
+++ src/components/Poll/Poll.js
@@ -1,47 +1,28 @@
 import React from 'react';
 
-const Poll = (props) => {
+const Poll = ({ header, open, isAccepted, author, datetimeTo }) => {
   let color;
-  if (props.open) {
+  if (open) {
     color = 'has-background-warning-light';
   } else {
-    if (props.isAccepted) {
+    if (isAccepted) {
       color = 'has-background-primary-light';
     } else {
       color = 'has-background-danger-light';
     }
   }
-
+  //TODO заменить текст на props когда придет реальные данные
   return (
     <div className={'card mt-2 ' + color}>
       <div className="card-content">
         <div className="media">
           <div className="media-content">
-            <p className="title is-4">{props.header}</p>
+            <p className="title is-4">{header}</p>
+            <p className="subtitle is-6">Автор: Иванов Иван{author}</p>
             <span className="tag is-info is-light">Ремонт</span>
+            <p className="subtitle is-7">Дата окончания : 1 Jan 2016 {datetimeTo}</p>
           </div>
         </div>
-        <div className="content">
-          Здесь будет находиться краткое описание голосовалки, но можно и без него.
-        </div>
-        <table className="table is-size-7">
-          <thead>
-            <tr>
-              <th>Дата создания</th>
-              <th>Дата окончания</th>
-            </tr>
-          </thead>{' '}
-          <tbody>
-            <tr>
-              <td>
-                <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-              </td>
-              <td>
-                <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-              </td>
-            </tr>
-          </tbody>
-        </table>
         <footer className="card-footer">
           <a className={'button mt-2 ' + color}>Открыть</a>
         </footer>
