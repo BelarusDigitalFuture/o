1cd615f7389f345541b99a2739d85dc810e6c396
Andrei Bezmen <andryshabez26@gmail.com>
2021-02-10 18:00:43 +0300

    fix: rebase conflict

--- src/shared/state/polls.js
+++ src/shared/state/polls.js
@@ -75,12 +75,21 @@ const defaultPolls = [
     header: 'Голосование за очень важный вопрос',
     date: new Date(2020, 11, 31),
     author: 'Председатель дома',
+<<<<<<< HEAD
     text: 'Чтож, давайте голосовать за очень важный вопрос!',
     isRadio: true,
     tags: ['официально', 'ремонт'],
     pollData: {
       question: 'Вопрос важный?',
       items: ['Очень важный', 'Архи важный!'],
+=======
+    text: 'Должны проголосовать не менее 50% жильцов',
+    isRadio: true,
+    tags: ['неофициально', 'праздник', 'дети'],
+    pollData: {
+      question: 'Вы согласны?',
+      items: ['Да', 'Нет'],
+>>>>>>> a880bb7... fix: rebase conflict
       results: [14, 27],
       discussionId: 1,
     },
@@ -115,6 +124,23 @@ const reducer = (state, action) => {
               : !action.poll.quorum && action.poll.tags.includes('официально')
               ? 0.5
               : 1,
+        },
+      ];
+    case 'REPEAT_POLL':
+      return [
+        ...state.filter((e) => e.id !== action.payload.id),
+        {
+          header: `${action.payload.header} (ПОВТОРНО)`,
+          date: new Date(2021, 1, 27),
+          author: action.payload.author,
+          text: action.payload.text,
+          isRadio: !action.payload.isRadio || true,
+          tags: action.payload.tags,
+          isAccepted: action.payload.isAccepted || false,
+          pollData: action.payload.pollData,
+          id: 4,
+          userAmount: action.payload.userAmount,
+          quorum: action.payload.quorum * 0.5,
         },
       ];
     default:
