124b1dbb302e10ab4ec890e38f45203ce21c5120
eug-alekseev <eugene.alekseev@kuehne-nagel.com>
2021-02-10 16:51:35 +0200

    handling default value in Select

--- src/shared/form/Select/Select.jsx
+++ src/shared/form/Select/Select.jsx
@@ -10,18 +10,19 @@ const SelectField = ({ options, field, form, isMulti, label }) => {
         );
       }
     : (option) => {
-        return form.setFieldValue(field.name, '' && option.value);
+        return form.setFieldValue(field.name, option ? option.value : '');
       };
 
   return (
     <div className="field">
       <label className="label">{label}</label>
       <Select
+        selectedValue={options ? options.find((option) => option.value === field.value) : ''}
         isClearable={true}
         options={options}
         name={field.name}
         isMulti={isMulti}
-        value={options ? options.find((option) => option.value === field.value) : ''}
+        value={options ? options.find((option) => option.value.toString() === field.value) : ''}
         onChange={onChange}
         onBlur={field.onBlur}
       />
