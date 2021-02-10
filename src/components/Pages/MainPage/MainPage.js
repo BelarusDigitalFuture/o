e3fb554fd689aa1c5872542380a82e25cfaf2d99
eug-alekseev <eug.alekseev@gmail.com>
2021-02-11 00:23:16 +0200

    Added initial graphs on the main page (no formatting yet)

--- src/components/Pages/MainPage/MainPage.js
+++ src/components/Pages/MainPage/MainPage.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import GenericPage from '../GenericPage/GenericPage';
+import FlourishGraph from '../../FlourishGraph';
 
-const MainPage = (props) => {
+const MainPage = () => {
   return (
     <GenericPage hasBackButton={false}>
-      <p>Here is an index page</p>
+      <div className="card">
+        {/*water*/}
+        <FlourishGraph src="visualisation/5260155" />
+      </div>
+      <div className="card">
+        {/*electricity*/}
+        <FlourishGraph src="visualisation/5259749" />
+      </div>
+      <div className="card">
+        {/*all utulities*/}
+        <FlourishGraph src="visualisation/5260078" />
+      </div>
     </GenericPage>
   );
 };
