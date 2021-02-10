24c0abe86e0b1e9e735c4281ce73c4145e3055d7
eug-alekseev <eugene.alekseev@kuehne-nagel.com>
2021-02-10 10:12:34 +0200

    Poll form created & polls reducer aligned with fields in the form

--- src/components/PollForm/PollForm.jsx
+++ src/components/PollForm/PollForm.jsx
@@ -3,37 +3,68 @@ import { useHistory } from 'react-router-dom';
 import { Field } from 'formik';
 import * as Yup from 'yup';
 import { AppForm } from '../../shared';
-import { TextInput, SelectField, CheckboxField } from '../../shared/form';
+import {
+  TextInput,
+  SelectField,
+  CheckboxField,
+  DatePickerField,
+  CreatableSelectField,
+} from '../../shared/form';
 import { TagsContext, PollsContext } from '../../shared/state';
+import GenericPage from '../Pages/GenericPage/GenericPage';
 
 const PollForm = () => {
   const { tags } = useContext(TagsContext);
   const { dispatch } = useContext(PollsContext);
   const history = useHistory();
-  const options = tags && tags.map(({ title }) => ({ label: title, value: title }));
+  const tagOptions = tags && tags.map(({ title }) => ({ label: title, value: title }));
   const handleFormSubmit = (values) => {
     dispatch({ type: 'ADD_POLL', poll: { ...values } });
     history.push('/polls');
   };
 
   const validationSchema = Yup.object({
-    header: Yup.string().max(50, 'Must be 50 characters or less').required('Required'),
-    text: Yup.string().required('Required'),
-    question: Yup.string().required('Required'),
+    header: Yup.string()
+      .max(50, 'Поле не должно быть длиннее 50 символов')
+      .required('Поле обязательно для заполнения'),
+    text: Yup.string().required('Поле обязательно для заполнения'),
+    question: Yup.string().required('Поле обязательно для заполнения'),
+    tags: Yup.array(),
+    date: Yup.date().required('Поле обязательно для заполнения'),
+    items: Yup.array()
+      .min(2, 'Должно быть как минимум два варианта ответа')
+      .required('Поле обязательно для заполнения'),
   });
 
   return (
-    <AppForm
-      initial={{ tagsss: [] }}
-      validationSchema={validationSchema}
-      onSubmit={handleFormSubmit}
-    >
-      <TextInput label={'Header'} name="header" />
-      <TextInput label={'Text'} name="text" />
-      <Field label="Tags" name={'tags'} isMulti component={SelectField} options={options} />
-      <CheckboxField name="multichoice">Multi</CheckboxField>
-      <TextInput label={'Question'} name="question" />
-    </AppForm>
+    <GenericPage>
+      <AppForm
+        initial={{ tags: [], header: '', text: '', date: '', question: '', items: [] }}
+        validationSchema={validationSchema}
+        onSubmit={handleFormSubmit}
+      >
+        <TextInput label={'Header'} name="header" />
+        <TextInput label={'Text'} name="text" />
+        <Field label="Tags" name={'tags'} isMulti component={SelectField} options={tagOptions} />
+        <Field
+          label="Дата проведения"
+          name="date"
+          component={DatePickerField}
+          customInput={<input className="input" type="search" />}
+        />
+        <CheckboxField label="Возможно выбрать несколько вариантов?" name="multichoice">
+          Multi
+        </CheckboxField>
+        <TextInput label={'Вопрос'} name="question" />
+        <Field
+          label="Варианты ответа"
+          name="items"
+          isMulti
+          component={CreatableSelectField}
+          // options={tagOptions}
+        />
+      </AppForm>
+    </GenericPage>
   );
 };
 
