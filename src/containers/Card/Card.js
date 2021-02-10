f8aaf4270f50061d01fb4748b6681a7480564216
Andrei Bezmen <andryshabez26@gmail.com>
2021-02-10 18:00:43 +0300

    fix: rebase conflict

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 const Card = ({
   id,
@@ -7,16 +6,19 @@ const Card = ({
   date = new Date(),
   author = '',
   tags = '',
-  pollData,
+  pollData = {},
   isTopic = false,
   isEvent = false,
   isPoll = false,
   isNew = false,
   onClick,
+  userAmount,
+  quorum,
 }) => {
   const [isHovered, setIsHovered] = useState(false);
-
+  const { results = [] } = pollData;
   const isOpen = date.getTime() >= new Date().getTime();
+  const isFailed = userAmount * quorum > results.reduce((ac, cur) => ac + cur, 0);
   let color = isHovered ? 'has-background-grey-lighter' : 'has-background-light';
   if (isNew) {
     color = isHovered ? 'has-background-primary' : 'has-background-primary-light';
@@ -24,6 +26,8 @@ const Card = ({
   if (isPoll) {
     if (isOpen) {
       color = isHovered ? 'has-background-warning' : 'has-background-warning-light';
+    } else if (isFailed) {
+      color = isHovered ? 'has-background-grey-light' : 'has-background-grey-lighter';
     } else {
       if (pollData.results.some((x) => x > 50)) {
         color = isHovered ? 'has-background-primary' : 'has-background-primary-light';
