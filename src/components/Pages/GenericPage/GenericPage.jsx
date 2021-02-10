664c86bab051e6ec932929deb327226934a6030f
eug-alekseev <eugene.alekseev@kuehne-nagel.com>
2021-02-10 14:31:38 +0200

    generic page everywhere

--- src/components/Pages/GenericPage/GenericPage.jsx
+++ src/components/Pages/GenericPage/GenericPage.jsx
@@ -3,11 +3,11 @@ import { useHistory } from 'react-router-dom';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const GenericPage = ({ header = '', children }) => {
+const GenericPage = ({ header = '', hasBackButton = true, children }) => {
   const history = useHistory();
   return (
-    <div className="section pt-5 mt-3">
-      <nav className="level">
+    <div className="section">
+      <nav className={'level ' + (hasBackButton ? '' : 'is-hidden')}>
         <div className="level-left">
           <div className="level-item">
             <button
