7211c781c06cf4e8d90e2ea89164566f9af97b32
eug-alekseev <eugene.alekseev@kuehne-nagel.com>
2021-02-10 16:18:03 +0200

    handling onChange event if isMulti set to false

--- src/shared/form/Select/Select.jsx
+++ src/shared/form/Select/Select.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import Select from 'react-select';
 
-const SelectField = ({ options, field, form, isMulti, label }) => (
-  <div className="field">
-    <label className="label">{label}</label>
-    <Select
-      options={options}
-      name={field.name}
-      isMulti={isMulti}
-      value={options ? options.find((option) => option.value === field.value) : ''}
-      onChange={(option) => {
+const SelectField = ({ options, field, form, isMulti, label }) => {
+  const onChange = isMulti
+    ? (option) => {
         return form.setFieldValue(
           field.name,
-          option.map((x) => x.value),
+          option.map((x) => x.value || ''),
         );
-      }}
-      onBlur={field.onBlur}
-    />
-  </div>
-);
+      }
+    : (option) => {
+        return form.setFieldValue(field.name, '' && option.value);
+      };
+
+  return (
+    <div className="field">
+      <label className="label">{label}</label>
+      <Select
+        isClearable={true}
+        options={options}
+        name={field.name}
+        isMulti={isMulti}
+        value={options ? options.find((option) => option.value === field.value) : ''}
+        onChange={onChange}
+        onBlur={field.onBlur}
+      />
+    </div>
+  );
+};
 
 export default SelectField;
