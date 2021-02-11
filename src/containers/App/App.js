7d68638019aa40b94435cd918dad282061413232
Vadzim Sasonkin <vadzim.sasonkin@gmail.com>
2021-02-11 16:36:27 +0000

    mobile menu

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,21 +4,6 @@ import { TagsProvider, TopicsProvider, PollsProvider, EventsProvider } from '../
 import AppContent from '../AppContent/AppContent';
 
 class App extends React.Component {
-  state = {
-    menuElements: [
-      { name: 'Главная', url: '/' },
-      { name: 'Мои данные', url: '/mydata' },
-      { name: 'Голосования', url: '/polls' },
-      { name: 'Встречи', url: '/events' },
-      { name: 'Обсуждения', url: '/discussions' },
-    ],
-    activeMenuElementId: 0,
-  };
-
-  menuElementClickHandler = (id) => {
-    this.setState({ activeMenuElementId: id });
-  };
-
   render() {
     return (
       <TagsProvider>
--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import './Menu.css';
 import Menu from '../../components/Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 
 const menuElements = [
   { name: 'Главная', url: '/' },
@@ -11,17 +14,50 @@ const menuElements = [
 
 const AppMenu = () => {
   const [activeId, setActiveId] = useState(0);
+  const [mobileOpened, setMobileOpened] = useState(false);
 
   const menuElementClickHandler = (id) => {
+    setMobileOpened(false);
     setActiveId(id);
   };
 
   return (
-    <Menu
-      elements={menuElements}
-      activeElementId={activeId}
-      onElementClick={menuElementClickHandler}
-    />
+    <>
+      <a
+        role="button"
+        className={`navbar-burger app-navbar-burger ${mobileOpened ? 'is-active' : ''}`}
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        onClick={() => setMobileOpened(!mobileOpened)}
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+      <aside className="column menu-class menu is-2 is-block-mobile is-inline is-fullheight section is-static is-hidden-touch	">
+        <Menu
+          elements={menuElements}
+          activeElementId={activeId}
+          onElementClick={menuElementClickHandler}
+        />
+      </aside>
+      <div className={`modal is-hidden-desktop ${mobileOpened ? 'is-active' : ''}`}>
+        <div className="modal-background has-background-white"></div>
+        <div className="modal-content is-fullheight modal-menu-content">
+          <Menu
+            elements={menuElements}
+            activeElementId={activeId}
+            onElementClick={menuElementClickHandler}
+          />
+        </div>
+        <button
+          className="modal-close is-large is-dark delete modal-menu-close has-background-primary"
+          aria-label="close"
+          onClick={() => setMobileOpened(false)}
+        ></button>
+      </div>
+    </>
   );
 };
 
