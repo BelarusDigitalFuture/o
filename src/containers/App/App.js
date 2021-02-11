ad975008edc75a9e0bf3a9c8ef465f48fa1272fa
eug-alekseev <eugene.alekseev@kuehne-nagel.com>
2021-02-11 14:19:24 +0200

    cleaning up App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,35 +3,18 @@ import './App.css';
 import { TagsProvider, TopicsProvider, PollsProvider, EventsProvider } from '../../shared/state';
 import AppContent from '../AppContent/AppContent';
 
-class App extends React.Component {
-  state = {
-    menuElements: [
-      { name: 'Главная', url: '/' },
-      { name: 'Мои данные', url: '/mydata' },
-      { name: 'Голосования', url: '/polls' },
-      { name: 'Встречи', url: '/events' },
-      { name: 'Обсуждения', url: '/discussions' },
-    ],
-    activeMenuElementId: 0,
-  };
-
-  menuElementClickHandler = (id) => {
-    this.setState({ activeMenuElementId: id });
-  };
-
-  render() {
-    return (
-      <TagsProvider>
-        <TopicsProvider>
-          <PollsProvider>
-            <EventsProvider>
-              <AppContent />
-            </EventsProvider>
-          </PollsProvider>
-        </TopicsProvider>
-      </TagsProvider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <TagsProvider>
+      <TopicsProvider>
+        <PollsProvider>
+          <EventsProvider>
+            <AppContent />
+          </EventsProvider>
+        </PollsProvider>
+      </TopicsProvider>
+    </TagsProvider>
+  );
+};
 
 export default App;
