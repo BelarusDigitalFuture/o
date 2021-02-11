9584d370151d8f07a917c67fe724540e0f220ec6
eug-alekseev <eug.alekseev@gmail.com>
2021-02-11 09:52:33 +0200

    added dummy data to index

--- src/components/Pages/MainPage/MainPage.js
+++ src/components/Pages/MainPage/MainPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import GenericPage from '../GenericPage/GenericPage';
 import FlourishGraph from '../../FlourishGraph';
 
 const MainPage = () => {
+  const history = useHistory();
   return (
     <GenericPage hasBackButton={false}>
+      <div className="notification is-warning is-light">
+        У вас не оплачены коммунальные платежи за январь&apos;21.{' '}
+        <a className="is-link" onClick={() => console.log('not implemented')}>
+          Перейти к оплате
+        </a>
+      </div>
+      <div className="notification is-warning is-light">
+        У вас есть непросмотренные голосования.{' '}
+        <a className="is-link" onClick={() => history.push('/polls')}>
+          Перейти к голосованиям
+        </a>
+      </div>
+
       <div className="columns">
-        <div className="column">
+        <div className="column" style={{ height: '50%' }}>
           {/*water*/}
           <FlourishGraph src="visualisation/5260155" />
         </div>
