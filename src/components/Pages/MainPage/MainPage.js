a3962fd255b480e7a47086dc56d46143f3b0e97a
eug-alekseev <eug.alekseev@gmail.com>
2021-02-11 08:58:54 +0200

    put graphs to columns

--- src/components/Pages/MainPage/MainPage.js
+++ src/components/Pages/MainPage/MainPage.js
@@ -5,17 +5,19 @@ import FlourishGraph from '../../FlourishGraph';
 const MainPage = () => {
   return (
     <GenericPage hasBackButton={false}>
-      <div className="card">
-        {/*water*/}
-        <FlourishGraph src="visualisation/5260155" />
-      </div>
-      <div className="card">
-        {/*electricity*/}
-        <FlourishGraph src="visualisation/5259749" />
-      </div>
-      <div className="card">
-        {/*all utulities*/}
-        <FlourishGraph src="visualisation/5260078" />
+      <div className="columns">
+        <div className="column">
+          {/*water*/}
+          <FlourishGraph src="visualisation/5260155" />
+        </div>
+        <div className="column">
+          {/*electricity*/}
+          <FlourishGraph src="visualisation/5259749" />
+        </div>
+        <div className="column">
+          {/*all utulities*/}
+          <FlourishGraph src="visualisation/5260078" />
+        </div>
       </div>
     </GenericPage>
   );
